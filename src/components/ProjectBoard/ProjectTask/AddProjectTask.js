321bdbe1b5c85bc11506feb998ac2d6419529916
Shradha Mhaske <46949105+mhaskeshradha1@users.noreply.github.com>
2020-11-02 19:21:43 -0500

    Add mini or Child Task

--- src/components/ProjectBoard/ProjectTask/AddProjectTask.js
+++ src/components/ProjectBoard/ProjectTask/AddProjectTask.js
@@ -1,7 +1,49 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
+import { connect } from "react-redux";
+import classnames from "classnames";
+import { addProjectTask } from "../../../actions/backlogActions";
+import PropTypes from "prop-types";
 
  class AddProjectTask extends Component {
+
+    constructor(props) {
+        super(props);
+        const { id } = this.props.match.params;
+    
+        this.state = {
+          summary: "",
+          acceptanceCriteria: "",
+          status: "",
+          priority: 0,
+          dueDate: "",
+          myprojectIdentifier: id,
+          errors: {}
+        };
+      
+      this.onChange = this.onChange.bind(this);
+      this.onSubmit = this.onSubmit.bind(this);
+    }
+// on change of task
+onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+  onSubmit(e) {
+    e.preventDefault();
+
+    const newTask = {
+      summary: this.state.summary,
+      acceptanceCriteria: this.state.acceptanceCriteria,
+      status: this.state.status,
+      priority: this.state.priority,
+      dueDate: this.state.dueDate
+    };
+    this.props.addProjectTask(
+      this.state.myprojectIdentifier,
+      newTask,
+      this.props.history
+    );
+  }
     render() {
         const {id}=this.props.match.params;
         return (
@@ -12,21 +54,42 @@ import { Link } from 'react-router-dom';
                     <Link to={`/projectBoard/${id}`} className="btn btn-light">
                         Back to Task Board
                     </Link>
-                    <h4 className="display-4 text-center">Add Task</h4>
+                    <h4 className="display-4 text-center">Add Mini/Child Task</h4>
                     <p className="lead text-center">Task Name + Task Code</p>
-                    <form>
+                    <form onSubmit = {this.onSubmit}>
                         <div className="form-group">
-                            <input type="text" className="form-control form-control-lg" name="summary" placeholder=" Task summary" />
+                            <input type="text" 
+                            className="form-control form-control-lg" 
+                            name="summary" 
+                            placeholder=" Task summary" 
+                            value={this.state.summary}
+                            onChange={this.onChange}
+                            />
                         </div>
                         <div className="form-group">
-                            <textarea className="form-control form-control-lg" placeholder="Acceptance Criteria" name="acceptanceCriteria"></textarea>
+                            <textarea className="form-control form-control-lg" 
+                            placeholder="Acceptance Criteria"
+                             name="acceptanceCriteria"
+                             value={this.state.acceptanceCriteria}
+                             onChange={this.onChange}
+                             >
+                             </textarea>
                         </div>
                         <h6>Due Date</h6>
                         <div className="form-group">
-                            <input type="date" className="form-control form-control-lg" name="dueDate" />
+                            <input type="date"
+                             className="form-control form-control-lg" 
+                             name="dueDate" 
+                             value={this.state.dueDate}
+                             onChange={this.onChange}
+                             />
                         </div>
                         <div className="form-group">
-                            <select className="form-control form-control-lg" name="priority">
+                            <select className="form-control form-control-lg"
+                             name="priority"
+                             value={this.state.priority}
+                             onChange={this.onChange}
+                             >
                                 <option value={0}>Select Priority</option>
                                 <option value={1}>High</option>
                                 <option value={2}>Medium</option>
@@ -35,7 +98,11 @@ import { Link } from 'react-router-dom';
                         </div>
 
                         <div className="form-group">
-                            <select className="form-control form-control-lg" name="status">
+                            <select className="form-control form-control-lg"
+                             name="status"
+                             value={this.state.status}
+                             onChange={this.onChange}
+                             >
                                 <option value="">Select Status</option>
                                 <option value="TO_DO">TO DO</option>
                                 <option value="IN_PROGRESS">IN PROGRESS</option>
@@ -52,4 +119,11 @@ import { Link } from 'react-router-dom';
         )
     }
 }
-export default AddProjectTask;
+AddProjectTask.propTypes = {
+    addProjectTask: PropTypes.func.isRequired
+  };
+  
+  export default connect(
+    null,
+    { addProjectTask }
+  )(AddProjectTask);
