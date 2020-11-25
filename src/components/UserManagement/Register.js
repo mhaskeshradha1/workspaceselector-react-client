26f311adc1ad8794eb4a415dc985567b6d92c766
Shradha Mhaske <46949105+mhaskeshradha1@users.noreply.github.com>
2020-11-24 22:03:50 -0500

    User Registration fro UI

--- src/components/UserManagement/Register.js
+++ src/components/UserManagement/Register.js
@@ -1,7 +1,57 @@
 import React, { Component } from 'react'
+import { createNewUser } from "../../actions/securityActions";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import classnames from "classnames";
 
- class Register extends Component {
+
+class Register extends Component {
+     
+    constructor() { 
+        super();
+        this.state = {
+            username: "",
+            fullName: "",
+            password: "",
+            confirmPassword: "",
+            errors: {}
+        };
+
+
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+    }
+
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.errors) {
+          this.setState({ errors: nextProps.errors });
+        }
+      }
+
+
+
+    onSubmit(e) {
+        e.preventDefault();
+        const newUser = {
+          username: this.state.username,
+          fullName: this.state.fullName,
+          password: this.state.password,
+          confirmPassword: this.state.confirmPassword
+        };
+    
+        this.props.createNewUser(newUser, this.props.history);
+      }
+    onChange(e) { 
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+
     render() {
+
+        const { errors } = this.state;
+
         return (
             <div className="register">
         <div className="container">
@@ -9,22 +59,70 @@ import React, { Component } from 'react'
                 <div className="col-md-8 m-auto">
                     <h1 className="display-4 text-center">Sign Up</h1>
                     <p className="lead text-center">Create your Account</p>
-                    <form action="create-profile.html">
+                            <form onSubmit={ this.onSubmit}>
                         <div className="form-group">
-                            <input type="text" className="form-control form-control-lg" placeholder="Name" name="name"
-                                required />
+                                    <input type="text" className={classnames("form-control form-control-lg", {
+                                        "is-invalid": errors.fullName
+                                      })}
+                                        placeholder="fullName" name="fullName"
+                                        value={this.state.fullName}
+                                        onChange={ this.onChange}
+                                    />
+
+                                    {errors.fullName && (
+                                        <div className="invalid-feedback">{errors.fullName}</div>
+                                      )}
                         </div>
                         <div className="form-group">
-                            <input type="email" className="form-control form-control-lg" placeholder="Email Address" name="email" />
+                        <input
+                        type="text"
+                        className={classnames("form-control form-control-lg", {
+                          "is-invalid": errors.username
+                        })}
+                                        placeholder="Email Address [username]" name="username"
+                                        value={this.state.username}
+                                        onChange={ this.onChange}
+                                        />
+                                        {errors.username && (
+                                            <div className="invalid-feedback">{errors.username}</div>
+                                          )}
+                                   
+                                    
 
                         </div>
                         <div className="form-group">
-                            <input type="password" className="form-control form-control-lg" placeholder="Password" name="password" />
+                        <input
+                        type="password"
+                        className={classnames("form-control form-control-lg", {
+                          "is-invalid": errors.password
+                        })}
+                                        placeholder="Password" name="password"
+                                        value={this.state.password}
+                                        onChange={ this.onChange}
+                                    />
+                                    {errors.password && (
+                                        <div className="invalid-feedback">{errors.password}</div>
+                                      )}
                         </div>
                         <div className="form-group">
-                            <input type="password" className="form-control form-control-lg" placeholder="Confirm Password"
-                                name="password2" />
-                        </div>
+                        <input
+                        type="password"
+                        className={classnames("form-control form-control-lg", {
+                          "is-invalid": errors.confirmPassword
+                        })}
+                                        placeholder="Confirm Password"
+                                        name="confirmPassword"
+                                        value={this.state.confirmPassword}
+                                        onChange={ this.onChange}
+                                    />
+
+                                    
+                                    {errors.confirmPassword && (
+                                        <div className="invalid-feedback">
+                                          {errors.confirmPassword}
+                                        </div>
+                                      )}
+                                    </div>
                         <input type="submit" className="btn btn-info btn-block mt-4" />
                     </form>
                 </div>
@@ -37,4 +135,15 @@ import React, { Component } from 'react'
         )
     }
 }
-export default Register;
+Register.propTypes = {
+    createNewUser: PropTypes.func.isRequired,
+    errors: PropTypes.object.isRequired
+  };
+  
+  const mapStateToProps = state => ({
+    errors: state.errors
+  });
+  export default connect(
+    mapStateToProps,
+    { createNewUser }
+  )(Register);
